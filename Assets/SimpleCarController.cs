3a5a2788a6fddff2af8fe120c373eb68d0085deb
Kadyn Eng <KadynE21@yorkhouse.ca>
2019-11-29 15:12:35 -0800

    Initial Unity Commit

---
+++ Assets/SimpleCarController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SimpleCarController : MonoBehaviour
+{
+	public List<AxleInfo> axleInfos; // the information about each individual axle
+	public float maxMotorTorque; // maximum torque the motor can apply to wheel
+	public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+	private float motor;
+	private float steering;
+
+	public void FixedUpdate()
+	{
+		motor = maxMotorTorque * Input.GetAxis("Vertical");
+		steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+		Debug.Log(steering);
+		Debug.Log(motor);
+
+		foreach (AxleInfo axleInfo in axleInfos)
+		{
+			if (axleInfo.steering)
+			{
+				axleInfo.leftWheel.steerAngle = steering;
+				axleInfo.rightWheel.steerAngle = steering;
+			}
+			if (axleInfo.motor)
+			{
+				axleInfo.leftWheel.motorTorque = motor;
+				axleInfo.rightWheel.motorTorque = motor;
+			}
+		}
+	}
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	public bool motor; // is this wheel attached to motor?
+	public bool steering; // does this wheel apply steer angle?
+}
